6ef858c51d493dfc4a96c0608880aa5073fac0dd
Sandyla Stocler <sanystocler@gmail.com>
2025-04-15 14:57:03 -0300

    cards cimentos

--- obj/Debug/net8.0/DepositoDoValeDoAco.AssemblyInfo.cs
+++ obj/Debug/net8.0/DepositoDoValeDoAco.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DepositoDoValeDoAco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37de9330150d5e6b4d4044723aecaa97010e031f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+791578a5ed4f6add884b6757c47f17badb1abe80")]
 [assembly: System.Reflection.AssemblyProductAttribute("DepositoDoValeDoAco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DepositoDoValeDoAco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
