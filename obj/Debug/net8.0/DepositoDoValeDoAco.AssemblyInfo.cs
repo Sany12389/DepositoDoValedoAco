ee2c02fbc2381cfe5110d9d204430ac120e90cea
Sandyla Stocler <sanystocler@gmail.com>
2025-04-15 15:53:22 -0300

    cards areia

--- obj/Debug/net8.0/DepositoDoValeDoAco.AssemblyInfo.cs
+++ obj/Debug/net8.0/DepositoDoValeDoAco.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DepositoDoValeDoAco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+791578a5ed4f6add884b6757c47f17badb1abe80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef858c51d493dfc4a96c0608880aa5073fac0dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DepositoDoValeDoAco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DepositoDoValeDoAco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
