5796fd18aa4cc82a59d6c9b4021b8ffd4b3bc1ff
Sandyla Stocler <sanystocler@gmail.com>
2025-04-15 16:18:57 -0300

    cards caixas daguas

--- obj/Debug/net8.0/DepositoDoValeDoAco.AssemblyInfo.cs
+++ obj/Debug/net8.0/DepositoDoValeDoAco.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DepositoDoValeDoAco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a49ba33407e411aa2756fbe29b2ab75bc7267d01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f72baf39d2d9eec8f5bfc59da52c0001ce9a22")]
 [assembly: System.Reflection.AssemblyProductAttribute("DepositoDoValeDoAco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DepositoDoValeDoAco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
